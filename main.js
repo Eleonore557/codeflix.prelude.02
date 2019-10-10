c2e6e5dd3d709d9638f74ac3986fd8f6d33a7881
Eléonore AYARI <20190998@c13-03.etudiants.campus.villejuif>
2019-10-10 09:24:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const Count = first('./ex01')
+const Count = last('./ex03')
+
+console.log(first([10, 5, 'hello', 2] ));
+console.log(first([10, 5, 'hello', 2], 2));
+console.log(first([10, 5, 'hello', 2], 3));
+
+console.log(last([10, 5, 'hello', 2] ));
+console.log(last([10, 5, 'hello', 2], 2));
+console.log(last([10, 5, 'hello', 2], 3));
